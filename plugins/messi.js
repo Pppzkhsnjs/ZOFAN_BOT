a4ad18ea2b1ad15ba1017a0a3d228167ba771919
Mahdia50 <106918349+Mahdia50@users.noreply.github.com>
2024-05-04 16:52:01 +0100

    Add files via upload

--- plugins/messi.js
+++ plugins/messi.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
-let res = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/Messi.json`)).data  
+let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/Messi.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-conn.sendFile(m.chat, url, 'error.jpg', `*Messi*`, m)} 
-//conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
-handler.help = ['messi']
-handler.tags = ['img']
-handler.command = /^(ميسي)$/i
+conn.sendFile(m.chat, url, 'error.jpg', `*المعزه بس كريستيانو عمه 01114329294*`, m)}
+//conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
+handler.help = ['مسي', 'ميسي']
+handler.tags = ['internet']
+handler.command = /^(ميسي|مسي)$/i
 export default handler
