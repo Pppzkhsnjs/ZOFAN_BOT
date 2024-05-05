693aea357f27dfc6c2001e8512e3fae28a777ef3
Mahdia50 <106918349+Mahdia50@users.noreply.github.com>
2024-05-05 14:16:05 +0100

    Update Animecouple.js

--- plugins/Animecouple.js
+++ plugins/Animecouple.js
@@ -5,9 +5,9 @@ let handler = async (m, { conn }) => {
   let cita = data[Math.floor(Math.random() * data.length)]
   
   let cowi = await(await fetch(cita.cowo)).buffer()
-  await conn.sendFile(m.chat, cowi, '', 'تطقيم ل ولد 👦 『 𝐬𝐚𝐬𝐮𝐤𝐞 𝐛𝐨𝐭 🩸 』', m)
+  await conn.sendFile(m.chat, cowi, '', 'تطقيم ل ولد 👦 『 𝒁𝑶𝑭𝑨𝑵 𝐛𝐨𝐭 🩸 』', m)
   let ciwi = await(await fetch(cita.cewe)).buffer()
-  await conn.sendFile(m.chat, ciwi, '', 'تطقيم ل بنت 👧 『 𝐬𝐚𝐬𝐮𝐤𝐞 𝐛𝐨𝐭 🩸 』', m)
+  await conn.sendFile(m.chat, ciwi, '', 'تطقيم ل بنت 👧 『 𝒁𝒐𝒇𝒂𝒏 𝐛𝐨𝐭 🩸 』', m)
 }
 handler.help = ['ppcouple', 'ppcp']
 handler.tags = ['internet']
