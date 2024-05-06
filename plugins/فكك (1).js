3c76b8794c03f4bbd860f464d3fc5851355479fe
Mahdia50 <106918349+Mahdia50@users.noreply.github.com>
2024-05-06 11:54:20 +0100

    Update فكك (1).js

--- plugins/فكك (1).js
+++ plugins/فكك (1).js
@@ -1,35 +1,39 @@
 let timeout = 60000
 let poin = 500
 let handler = async (m, { conn, command, usedPrefix }) => {
-    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    conn.fakk = conn.fakk ? conn.fakk : {}
     let id = m.chat
-    if (id in conn.tebakbendera) {
-        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
+    if (id in conn.fakk) {
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.fakk[id][0])
         throw false
     }
-    let tekateki = JSON.parse(fs.readFileSync(`./src/game/miku2.json`));
-    let json = tekateki[Math.floor(Math.random() * tekateki.length)];
-    let _clue = json.response;
-    let clue = _clue.replace(/[A-Za-z]/g, ''); // Fixed this line
-    let caption = `
-ⷮ ${json.question}
-
-❐↞┇الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)}┇
-❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
-『 Zofan-𝘽𝙊𝙏』
-`.trim();
-    conn.tekateki[id] = [
-       await conn.reply(m.chat, caption, m),
-        json, poin,
-        setTimeout(async () => {
-            if (conn.tekateki[id]) await conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n ❐↞┇الاجـابـة✅↞ ${json.response}┇`, conn.tekateki[id][0]);
-            delete conn.tekateki[id];
+    let src = await (await fetch('https://api.jikan.moe/v3/top/characters/1')).json()
+    let character = src.top[Math.floor(Math.random() * src.top.length)]
+    let caption = `*${command.toUpperCase()}*
+  ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
+  *استخدم .انسحب للأنسحاب*
+  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
+『ℤ𝕆𝔽𝔸ℕ🤖』
+     `.trim()
+    conn.fakk[id] = [
+        await conn.sendFile(m.chat, character.image_url, '', caption, m),
+        character, poin,
+        setTimeout(() => {
+            if (conn.fakk[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${character.name}*┇`, conn.fakk[id][0])
+            delete conn.fakk[id]
         }, timeout)
-    ];
-};
+    ]
+}
 
-handler.help = ['miku'];
-handler.tags = ['game'];
-handler.command = /^(فكك)$/i;
+handler.help = ['fakk']
+handler.tags = ['game']
+handler.command = /^فكك/i
 
-export default handler;
+const answers = {
+    "1": "لوفي",
+    "2": "ناروتو",
+    "3": "جوجو",
+    // وهكذا
+}
+
+export { handler, answers }
