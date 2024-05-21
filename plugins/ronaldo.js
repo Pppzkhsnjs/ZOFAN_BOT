c6a4c7d04c34e3dc3e60c0ffe1f525c6a2a576e6
Pppzkhsnjs <dhbdbxjhehd@gmail.com>
2024-05-21 19:42:34 +0100

    Update ronaldo.js

--- plugins/ronaldo.js
+++ plugins/ronaldo.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let cristiano = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/CristianoRonaldo.json`)).data  
 let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
-conn.sendFile(m.chat, ronaldo, 'error.jpg', `*عمك كريستيانو رونالدو سييييييي 01114329294*`, m)}
+conn.sendFile(m.chat, ronaldo, 'error.jpg',  '  زوافان يحب رونالدو*`, m)}
 //conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `${usedPrefix + command}`]], m)}
 handler.help = ['cristianoronaldo', 'cr7']
 handler.tags = ['internet']
