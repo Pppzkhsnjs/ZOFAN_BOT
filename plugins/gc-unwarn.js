5ab95d4383c47a30cc9ba61d7041685c2befa46c
Pppzkhsnjs <dhbdbxjhehd@gmail.com>
2024-05-22 01:05:30 +0100

    Update gc-unwarn.js

--- plugins/gc-unwarn.js
+++ plugins/gc-unwarn.js
@@ -1,26 +1,19 @@
 
+let handler = async (m, { conn, args, groupMetadata}) => {
+       let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+       if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+       let warn = global.db.data.users[who].warn
+       let name = conn.getName(who)
+      m.reply(`
+ *WARNINGS*
 
-const handler = async (m, {conn, text, command, usedPrefix}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
-  const tradutor = _translate.plugins.gc_unwarn
+▢ *Name :* ${name} 
+▢ *Warns :* ${warn}`)
+}
 
-  const pp = './src/warn.jpg';
-  let who;
-  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text;
-  else who = m.chat;
-  const user = global.db.data.users[who];
-  const bot = global.db.data.settings[conn.user.jid] || {};
-  const warntext = `${tradutor.texto1}\n*${usedPrefix + command} @${global.suittag}*`;
-  if (!who) throw m.reply(warntext, m.chat, {mentions: conn.parseMention(warntext)});
-  if (m.mentionedJid.includes(conn.user.jid)) return;
-  if (user.warn == 0) throw tradutor.texto2;
-  user.warn -= 1;
-  await m.reply(`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `♻️ *@${who.split`@`[0]}*`}${tradutor.texto3} ${user.warn}/3*`, null, {mentions: [who]});
-};
-handler.command = /^(unwarn|delwarn|deladvertir|deladvertencia|delwarning)$/i;
-handler.group = true;
-handler.admin = true;
-handler.botAdmin = true;
-export default handler;
+handler.help = ['warns']
+handler.tags = ['group']
+handler.command = ['warns'] 
+handler.group = true
+
+export default handler
