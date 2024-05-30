86b9be59d98211205a47d87fe2bcf6b813b73379
Pppzkhsnjs <dhbdbxjhehd@gmail.com>
2024-05-30 22:02:12 +0100

    Update زوافان .js

--- plugins/زوافان .js
+++ plugins/زوافان .js
@@ -1,17 +1,17 @@
 // كود تم نشره بواسطه obito
 // تابع لقناه https://whatsapp.com/channel/0029VaSQX1TI1rcbxtQZ5518
 let handler = async (m, { conn }) => {
-let done = '♥️'; 
+let done = '🩶'; 
        m.react(done);
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message =  'عمك';
+    let message = `هنا  تحط النص اللي بدك ياه`;
 
-    conn.sendFile(m.chat, 'تحط  هنا تليجرام', 'image.jpg', message, m);
+    conn.sendFile(m.chat, 'تحط الصوره هنا تليجرام', 'image.jpg', message, m);
 };
 
-handler.customPrefix = /^(شينسوي|زوافان|كرولو|ريكي)$/i;
+handler.customPrefix = /^(bot|بوت)$/i;
 handler.command = new RegExp;
 
 export default handler;
