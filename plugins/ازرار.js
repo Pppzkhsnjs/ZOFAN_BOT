20af4b8cac2ee9ab51d2e68b40794a5b54912d1b
Pppzkhsnjs <dhbdbxjhehd@gmail.com>
2024-06-09 19:38:29 +0100

    Update ازرار.js

--- plugins/ازرار.js
+++ plugins/ازرار.js
@@ -40,106 +40,145 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
   						  					{
   						  		    		header: 'Test',
   										      title: 'Click Me',
-  									    	  description: '*
-           *┨══┅══┉▕🌀▏┉══┅══┠*
-┃ *↵ اهلا يا بك "امبرطور شرالشيطاني زوافان" في بوت  زوافان "*
-                                                              *┨══┅══┉▕معلوماتي▏┉══┅══┠*
-┇اسم البوت🤖: *ايس 
-┇اسم المالك👑: *زوافان*
+  									    	  description: 'Click Me',
+  								    		  id: 'tes'
+  						  		    	}
+  						  				]
+  						  			},
+  						  			{
+  						  				highlight_label: 'ON',
+  						  				rows: [
+  						  					{
+  						  		    		header: 'Test',
+  										      title: 'Click Me',
+  									    	  description: 'Click Me',
+  								    		  id: 'te'
+  						  		    	}
+  						  				]
+  						  			}
+  						  		]
+  						  	})
+  							},
+                              {
+                                  name: 'quick_reply',
+                                  buttonParamsJson: JSON.stringify({
+                                      display_text: 'quick_reply',
+                                      id: `message`
+                                  })
+                              },
+                              {
+                                  name: 'cta_url',
+                                  buttonParamsJson: JSON.stringify({
+                                      display_text: 'url',
+                                      url: 'https://www.google.com',
+                                      merchant_url: ''
+                                  })
+                              },
+                              {
+                                  name: 'cta_call',
+                                  buttonParamsJson: JSON.stringify({
+                                      display_text: 'call',
+                                      id: 'message'
+                                  })
+                              },
+                              {
+                                  name: 'cta_copy',
+                                  buttonParamsJson: JSON.stringify({
+                                      display_text: 'copy',
+                                      id: '123456789',
+                                      copy_code: 'message'
+                                  })
+                              },
+                              {
+                                  name: 'cta_reminder',
+                                  buttonParamsJson: JSON.stringify({
+                                      display_text: 'cta_reminder',
+                                      id: 'message'
+                                  })
+                              },
+                              {
+                                  name: 'cta_cancel_reminder',
+                                  buttonParamsJson: JSON.stringify({
+                                      display_text: 'cta_cancel_reminder',
+                                      id: 'message'
+                                  })
+                              },
+                              {
+                                  name: 'address_message',
+                                  buttonParamsJson: JSON.stringify({
+                                      display_text: 'address_message',
+                                      id: 'message'
+                                  })
+                              },
+                              {
+                                  name: 'send_location',
+                                  buttonParamsJson: JSON.stringify({
+                                  })
+                              }
+  			  		],
+                messageParamsJson: ''
+            }
+        };        
 
-╭*┨══┅══┉▕اوامري▏┉══┅══┠*
-*حط نقطه قبل اي ام
-   *┨══┅══┉▕قسم الجروابات▏┉══┅══┠*
-*انذار*
-*الغاء-الانذار*
-*احذف*
-*ابلاغ*
-*الجروب*
-*الجروبات*
-*دعوه*
-*تغير-الوصف*
-*تغير-الاسم*
-*تغير-الترحيب*
-*تغير-الصوره*
-*تغير-اللينك*
-*الدعم*
-*تغير-الوداع*
-*تغير-الترحيب*
-*منشن*
-*مخفي*
-*منشني*
-*جروب فتح*
-*جروب غلق*
-*لينك*
-*المشرفين*
-*الاشباح*
-╭━━━[ *صور 🖼️* ]━━━━⬣
-*رونالدو🕵️*
-*ميسي🕵️*
-*طقم🕵️*
+        let msg = generateWAMessageFromContent(m.chat, {
+            viewOnceMessage: {
+                message: {
+                    interactiveMessage,
+                },
+            },
+        }, { userJid: conn.user.jid, quoted: m })
+        conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id});
 
-╭━━━[ *قسم المطور 🤴🏻* ]━━━━⬣
-*بريم🌀*
-*اعفاء-التميز🌀*
-*اضف_اكسبي🌀*
-*اضف-الماس🌀*
-*حظر🌀*
-*المحظورين🌀*
-*بان🌀*
-*بلوك*
-*رفع-بلوك🌀*
-*البلوكات🌀*
-*فك-الحظر🌀*
-*انضم🌀*
-*اخرج🌀*
-*فك-البان🌀*
-╭━━━[ *اوامر المرح 🎡* ]━━━━⬣
-╭━━━[ *قسم العاب 🧿* ]━━━━⬣
-*احزر🧿*
-*علم🧿*
-*عين🧿*
-* انمي🧿*
-*رياضيات🧿*
-*اكس او🧿*
-*لعبه🧿*
-*صيد🧿*
-*دحيح🧿*
-*مسابقة 🧿*
-*اكس او 🧿*
-*كت🧿*
-*اديت🧿*
-*كوره🧿*
-*دين🧿*
-*كت.🧿*
-*رتب🧿*
-*ايموجي🧿*
-*هل🧿*
-*لو🧿*
-*سؤال🧿*
-*خمن🧿*
+    } else {
+        conn.sendFile(m.chat, m);      
+    }    
+};
+handler.help = ['اوامر'];
+handler.tags = ['ازرار'];
+handler.command = /^(mboton)$/i;
+export default handler//Copyright ©JOANIMI/KILLUA
+//https://whatsapp.com/channel/0029Vab5oDNElagpHtJjmT0B
 
+import { prepareWAMessageMedia, generateWAMessageFromContent, getDevice } from '@whiskeysockets/baileys'
 
+const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
+    const device = await getDevice(m.key.id);
 
-
-╭━━━[ *اوامر الملصقات 💫* ]━━━━⬣
-*دمج🐦*
-*ملصق🐦*
-*حقوق🐦*
-╭━━━[ *اوامر الدين 💓* ]━━━━⬣
-*🕋دين*
-*🕋ايات*
-*🕋اسماء-الله*
-*🕋اذكار الصباح*
-*🕋اذكار المساء*
-  
-
-*ملاحظة مهمة*
-
-*❶ - يمنع منعا بتا سب البوت.*
-
-~⌬ تــ✍︎ــوقــيــعي
-* 𝑍𝑂𝐹𝐴𝑁 𝐵𝑂𝑇 *
-‬',
+    if (device !== 'desktop' || device !== 'web') {      
+        const interactiveMessage = {
+            body: { text: `test`.trim() },
+            footer: { text: `©JoAnimi for test`.trim() },  
+            header: {
+                title: `test`,
+                subtitle: `test`,
+                hasMediaAttachment: false,
+            },
+            nativeFlowMessage: {
+  						buttons: [
+  							{
+  								name: 'single_select',
+  						  	buttonParamsJson: JSON.stringify({
+  						  		title: 'Click Me',
+  						  		sections: [
+  						  			{
+  						  				title: 'List',
+  							  	    highlight_label: 'ON',
+  						  		    rows: [
+  						  		    	{
+  						  		    		header: 'دوس',
+  										      title: 'Click Me',
+  									    	  description: 'هلا',
+  								    		  id: 'test'
+  						  		    	}
+  						  		    ]
+  						  			},
+  						  			{
+  						  				highlight_label: 'ON',
+  						  				rows: [
+  						  					{
+  						  		    		header: 'Test',
+  										      title: 'Click Me',
+  									    	  description: 'Click Me',
   								    		  id: 'tes'
   						  		    	}
   						  				]
